50d27b46084ca6d61cf4f26e653a6e62e1519827
mat777010m <62897972+mat777010m@users.noreply.github.com>
2021-02-06 14:18:13 +0100

    Master

---
+++ Launcher/DB.cs
@@ -0,0 +1,34 @@
+﻿/*using MySql.Data.MySqlClient;
+
+namespace Launcher
+{
+    class DB
+    {
+        private MySqlConnection connection = new MySqlConnection("Server=sql301.epizy.com;Port=3306;Database=epiz_26887928_launcherFG;Uid=epiz_26887928;Pwd=VO8DQMKY7l");
+
+        public void openConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+        }
+
+        // create a function to close the connection
+        public void closeConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+
+        // create a function to return the connection
+        public MySqlConnection getConnection()
+        {
+            return connection;
+        }
+
+    }
+}
+*/
---
+++ Launcher/MainWindow.xaml.cs
@@ -0,0 +1,120 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Launcher
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+
+
+        private void Rectangle_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void EndBtn_Click(object sender, RoutedEventArgs e)
+        {
+            bool closeAnim = true;
+            for (; ; )
+            {
+                {
+                    while (closeAnim == true)
+                    {
+                        if (Opacity >= 0.0)
+                        {
+                            Opacity -= 0.050;
+                        }
+                        else if (this.Opacity <= 0)
+                        {
+                            Close();
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+        private void MenuBtn_o_Click(object sender, RoutedEventArgs e)
+        {
+            MenuBtn_c.Visibility = Visibility.Visible;
+            MenuBtn_o.Visibility = Visibility.Hidden;
+        }
+
+        private void MenuBtn_c_Click(object sender, RoutedEventArgs e)
+        {
+            MenuBtn_c.Visibility = Visibility.Hidden;
+            MenuBtn_o.Visibility = Visibility.Visible;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            MenuBtn_c.Visibility = Visibility.Hidden;
+            MenuBtn_o.Visibility = Visibility.Visible;
+            Main.Content = new Page1();
+            SCPCARD.Visibility = Visibility.Collapsed;
+            SCPBTN.Visibility = Visibility.Collapsed;
+            SCPLABEL.Visibility = Visibility.Collapsed;
+        }
+
+        private void Rectangle_MouseDown2(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MenuBtn_c.Visibility = Visibility.Hidden;
+            MenuBtn_o.Visibility = Visibility.Visible;
+            Main.Content = new Biblioteka();
+            SCPCARD.Visibility = Visibility.Visible;
+            SCPBTN.Visibility = Visibility.Visible;
+            SCPLABEL.Visibility = Visibility.Visible;
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            MenuBtn_c.Visibility = Visibility.Hidden;
+            MenuBtn_o.Visibility = Visibility.Visible;
+            SCPCARD.Visibility = Visibility.Collapsed;
+            SCPBTN.Visibility = Visibility.Collapsed;
+            SCPLABEL.Visibility = Visibility.Collapsed;
+            Main.Content = new Sklep();
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            MenuBtn_c.Visibility = Visibility.Hidden;
+            MenuBtn_o.Visibility = Visibility.Visible;
+            Login Login = new Login();
+            Login.Show();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Page1();
+            SCPCARD.Visibility = Visibility.Collapsed;
+            SCPBTN.Visibility = Visibility.Collapsed;
+            SCPLABEL.Visibility = Visibility.Collapsed;
+            
+        }
+
+        private void MinBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SCPBTN_click(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new SCPpageinfo();
+            SCPBTN.Visibility = Visibility.Collapsed;
+            SCPCARD.Visibility = Visibility.Collapsed;
+            SCPLABEL.Visibility = Visibility.Collapsed;
+
+        }
+    }
+}
